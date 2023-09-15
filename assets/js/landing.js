6e3f6b2fb2dd77b540390033b9d1f5aab3e54d3b
Farkhandika12b <142876608+Farkhandika12b@users.noreply.github.com>
2023-09-15 20:48:33 +0700

    [fix] ubah header

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,3 +1,16 @@
-function tampilkanPopUp() {
-    alert("Halo! Ini adalah pop-up.");
+function toggleDropdown() {
+    var dropdown = document.getElementById("myDropdown");
+    var dropbtn = document.querySelector(".dropbtn");
+
+    if (dropdown.style.display === "block") {
+        dropdown.style.animation = 'fadeOut 0.3s';
+        setTimeout(function() {
+            dropdown.style.display = "none";
+            dropbtn.style.display = "inline-block"; // Mengubah dari "block" ke "inline-block"
+        }, 300);
+    } else {
+        dropdown.style.animation = 'fadeIn 0.3s';
+        dropdown.style.display = "block";
+        dropbtn.style.display = "none";
+    }
 }
