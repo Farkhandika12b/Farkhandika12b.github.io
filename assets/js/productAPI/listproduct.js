b292b4c295f657eed13ce7ccef71061e904d7a73
Farkhandika12b <142876608+Farkhandika12b@users.noreply.github.com>
2023-10-02 00:49:12 +0700

    [fix] productapi dan lain2

---
+++ assets/js/productAPI/listproduct.js
@@ -0,0 +1,37 @@
+// mengambil data dari API
+fetch('https://crudcrud.com/api/0162ae9101ca4c80a7fb13ac5f984fc7/list-products')
+    .then(response => response.json())
+    .then(data => {
+        var products = data; // Simpan data dari API ke dalam variabel products
+
+        // menampilkan produk di dalam halaman HTML
+        function displayProducts() {
+            var productListContainer = document.querySelector('.produk-list');
+
+            // Iterasi array produk
+            products.forEach(function(product) {
+                var productElement = document.createElement('div');
+                productElement.classList.add('produk');
+
+                productElement.innerHTML = `
+                    <p class="produk-title">${product.title}</p>
+                    <img src="${product.imageSrc}" alt="${product.title}">
+                    <div class="price-cart">
+                        <div class="price-container">
+                            <h5>Price</h6>
+                            <p id="price">${product.price}</p>
+                        </div>
+                        <p></p>
+                        <div class="cart-container">
+                            <a href=""><img src="../assets/cart.png" alt="cart" class="cart"></a>
+                        </div>
+                    </div>
+                `;
+
+                productListContainer.appendChild(productElement);
+            });
+        }
+
+        displayProducts();
+    })
+    .catch(error => console.error('Error:', error));
