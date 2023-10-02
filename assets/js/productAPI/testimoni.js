d836a7e820ca08330983fc58cd45a52a29e30963
Farkhandika12b <142876608+Farkhandika12b@users.noreply.github.com>
2023-10-02 18:15:43 +0700

    [fix] testimoni

--- assets/js/productAPI/testimoni.js
+++ assets/js/productAPI/testimoni.js
@@ -1,46 +1,37 @@
-//testimoni pelanggan
-const testimonials = [
-    {
-        name: "X_Player",
-        rating: 5,
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis elit sit amet odio malesuada ultrices.",
-    },
-    {
-        name: "Varthrax069",
-        rating: 4,
-        content: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
-    },
-    {
-        name: "HeavenSmith",
-        rating: 3,
-        content: "Aliquam ac mauris non ligula sagittis dignissim. Morbi vel nisi et ex mollis tempus.",
-    },
-];
+// testimoni.js
 
 const testimonialSlider = document.getElementById("testimonial-slider");
 
-testimonials.forEach(testimonial => {
-    const testimonialDiv = document.createElement("div");
-    testimonialDiv.classList.add("testimonial");
+function loadTestimonials() {
+    // Memuat data testimoni dari API
+    fetch('https://651a8471340309952f0d72a6.mockapi.io/testimonial')
+        .then(response => response.json())
+        .then(data => {
+            // Memproses data testimoni dari API
+            data.forEach(testimonial => {
+                const testimonialDiv = document.createElement("div");
+                testimonialDiv.classList.add("testimonial");
 
-    const testimonialImage = document.createElement("img");
-    testimonialImage.src = "/assets/user-icon.png"; // Ganti dengan URL gambar profil pelanggan
+                const testimonialImage = document.createElement("img");
+                testimonialImage.src = testimonial.image; // Ganti dengan URL gambar profil pelanggan dari API
 
-    const testimonialContent = document.createElement("div");
-    testimonialContent.classList.add("content");
-    testimonialContent.textContent = testimonial.content;
+                const testimonialContent = document.createElement("div");
+                testimonialContent.classList.add("content");
+                testimonialContent.textContent = testimonial.content;
 
-    const testimonialRating = document.createElement("div");
-    testimonialRating.classList.add("rating");
-    testimonialRating.innerHTML = "&#9733;".repeat(testimonial.rating);
+                const testimonialRating = document.createElement("div");
+                testimonialRating.classList.add("rating");
+                testimonialRating.innerHTML = "&#9733;".repeat(testimonial.rating);
 
-    testimonialDiv.appendChild(testimonialImage);
-    testimonialDiv.appendChild(testimonialContent);
-    testimonialDiv.appendChild(testimonialRating);
+                testimonialDiv.appendChild(testimonialImage);
+                testimonialDiv.appendChild(testimonialContent);
+                testimonialDiv.appendChild(testimonialRating);
 
-    testimonialSlider.appendChild(testimonialDiv);
-});
-
+                testimonialSlider.appendChild(testimonialDiv);
+            });
+        })
+        .catch(error => console.error('Error:', error));
+}
 
 function scrollTestimonials(direction) {
     const slider = document.getElementById("testimonial-slider");
@@ -50,3 +41,6 @@ function scrollTestimonials(direction) {
         behavior: "smooth"
     });
 }
+
+// Memanggil fungsi untuk memuat testimoni dari API
+loadTestimonials();
